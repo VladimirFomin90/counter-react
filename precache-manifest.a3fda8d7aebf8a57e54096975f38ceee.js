aae1c3dbe3941464291b9a29be03a0e6a97ac74f
VladimirFomin90 <vladimirfomin90@yandex.ru>
2022-10-11 21:51:02 +0600

    Updates

---
+++ precache-manifest.a3fda8d7aebf8a57e54096975f38ceee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e587b6efd9f77fbd8cb7",
+    "url": "/counter-react/static/js/main.e587b6ef.chunk.js"
+  },
+  {
+    "revision": "0badd271d447e5b33873",
+    "url": "/counter-react/static/js/1.0badd271.chunk.js"
+  },
+  {
+    "revision": "90ba072e34f32270a5a3",
+    "url": "/counter-react/static/js/runtime~main.90ba072e.js"
+  },
+  {
+    "revision": "57120a9e00443a3c1b366daed824fd4a",
+    "url": "/counter-react/index.html"
+  }
+];
